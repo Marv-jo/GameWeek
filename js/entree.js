8c4e04c6181740711f20217867d38aa1408b3361
Slevron <s.levron.pro@gmail.com>
2014-04-15 17:09:30 +0200

    Test

--- js/entree.js
+++ js/entree.js
@@ -1,8 +1,8 @@
 function Entree(game) {
     this.game = game;
     this.x = 200;
-    this.y = 200;
+    this.y = 288;
     this.sprite = game.add.sprite(this.x, this.y, 'entree');
-    this.sprite.animations.add('walk');
-    this.sprite.animations.play('walk', 20, false);
+    this.sprite.animations.add('open');
+    this.sprite.animations.play('open', 20, false);
 }
