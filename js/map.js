3096d4b34ed9804e46ce401c9faa8ccf8c24d746
Slevron <s.levron.pro@gmail.com>
2014-04-16 10:39:07 +0200

    Revert "objet map"
    This reverts commit 3906413acd780fb0d57eda1a8a60064e82ca5bab.

--- js/map.js
+++ js/map.js
@@ -1,22 +1,6 @@
 function Map(game) {
 	this.game = game;
-    this.game.action = false;
 	
-    this.game.plateforme = this.game.add.group();
-    this.game.map.createFromObjects('Calque objets 1', 32, 'plateforme', 0, true, false, this.game.plateforme);
-    //  Add animations to all of the entree sprites
-    if(this.game.action == true){
-        this.game.plateforme.callAll('animations.add', 'animations', 'plateforme', [0, 1, 2, 3], 10, false);
-        this.game.plateforme.callAll('animations.play', 'animations', 'plateforme');
-    }
-    this.game.plateforme2 = this.game.add.group();
-    this.game.map.createFromObjects('Calque objets 1', 35, 'plateforme2', 0, true, false, this.game.plateforme2);
-//  Add animations to all of the entree sprites
-    if(this.game.action == true){
-        this.game.plateforme2.callAll('animations.add', 'animations', 'plateforme2', [0, 1, 2, 3], 10, false);
-        this.game.plateforme2.callAll('animations.play', 'animations', 'plateforme2');
-    }
-
     this.game.entree = this.game.add.group();
     this.game.map.createFromObjects('Calque objets 1', 24, 'entree', 0, true, false, this.game.entree);
     //  Add animations to all of the entree sprites
